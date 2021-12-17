fa1fd2a7770d13b921ef5f1ad8fef94ea3bf9baf
Adityachess <89146150+Adityachess@users.noreply.github.com>
2021-12-17 18:15:35 +0530

    Added:Rest call using post method.

--- src/main/java/com/bridgelabz/restapicalls/controller/RestController.java
+++ src/main/java/com/bridgelabz/restapicalls/controller/RestController.java
@@ -1,14 +1,27 @@
 package com.bridgelabz.restapicalls.controller;
 
+import com.bridgelabz.restapicalls.model.User;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
 @org.springframework.web.bind.annotation.RestController
-
-
 @RequestMapping("/hello")
+/** 
+ * @RestController:to create the rest controller class.
+ * @RequestMapping:describe the URL.
+ * @RequestParam:to pass the value as parameter.
+ * @GetMapping:to get the name as a parameter.
+ * @PostMpping:to post the first name and last name as parameter
+ */
 public class RestController {
     @RequestMapping(value={"", "/","home"})
     public String sayHello(){
@@ -27,5 +40,8 @@ public class RestController {
     public String sayHelloPara(@PathVariable String name) {
         return "Hello " + name + " From Bridgelabz";
     }
-    
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " From Bridgelabz";
+    }
 }
